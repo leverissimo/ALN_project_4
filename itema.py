0f2f2da8f02bbdeac2c8afbd59430f7c940d7d47
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2025-06-14 11:41:38 -0300

    add generate gaussian matrix function

---
+++ itema.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def generate_gaussian_matrix(M: int, N:int)->np.ndarray:
+    """Cria uma matriz Gaussiana com as entradas
+    seguindo uma distribuição normal com media 0 e 
+    variancia igual a 1 
+
+    Args:
+        M (int): Dimensão das linhas da Matriz
+        N (int): Dimensão das colunas da Matriz
+
+    Returns:
+        np.ndarray: Matriz Gaussiana
+    """
+    
+    matriz = np.random.normal(loc=0, scale = 1, size=(M, N))
+    
+    return matriz
